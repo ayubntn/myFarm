cc3a625274aaea92c9c602637c8744c64916f925
ayukoyamashita <ayuko.yamashita@kufusumai.co.jp>
2024-01-03 18:24:36 +0900

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
+import Phaser from "phaser";
+import MyScene from "./scenes/MyScene";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const canvas = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!canvas) return;
+
+    const config: Phaser.Types.Core.GameConfig = {
+      canvas: canvas.current!,
+      customEnvironment: true,
+      type: Phaser.CANVAS,
+      width: 800,
+      height: 600,
+      physics: {
+        default: "arcade",
+        arcade: {
+          gravity: { y: 200 },
+        },
+      },
+      scene: MyScene,
+    };
+    new Phaser.Game(config);
+  }, [canvas]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <canvas ref={canvas} />
     </>
   )
 }
